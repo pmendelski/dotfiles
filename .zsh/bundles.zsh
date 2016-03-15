045eb8869a55e694f9a0ee86eb08c89aa14a908f
mendlik <mendelski.pawel@gmail.com>
2016-03-15 21:19:11 +0100

    Fixed autosuggestion in zsh

--- .zsh/bundles.zsh
+++ .zsh/bundles.zsh
@@ -5,6 +5,10 @@ source $DIR/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Load zsh-autosuggestions.
 source $DIR/bundle/autosuggestions/zsh-autosuggestions.zsh
+# Menu + autosuggestion issue
+# https://github.com/zsh-users/zsh-autosuggestions/issues/118
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=("expand-or-complete")
+# Enable better colors
 if [[ $(echotc Co) -gt 8 ]];then
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=238"
 else
