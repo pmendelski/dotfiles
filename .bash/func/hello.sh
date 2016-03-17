897438a924b4821fa671c47d103c85803070e1b5
mendlik <mendelski.pawel@gmail.com>
2016-03-17 21:29:09 +0100

    Minor fix in hello msg

--- .bash/func/hello.sh
+++ .bash/func/hello.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function shhello() {
-    last $USER | grep "$(date +"%a %b ")17" | grep -qv "$(tty | sed "s:/dev/::").* still logged in"
-    if [ $? -eq 0 ]; then
+    last $USER | grep "$(date +"%a %b %d")" | grep -qv "$(tty | sed "s:/dev/::").* still logged in"
+    if [ $? -ne 0 ]; then
         dailyepigram
         echo ""
         echo "User: $USER@${HOSTNAME:$HOST}"
