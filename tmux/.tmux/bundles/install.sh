8a00a265724dd4bee7ca4885cdb76d1ddba76dca
mendlik <mendelski.pawel@gmail.com>
2016-11-05 16:08:54 +0100

    Add tmux plugins

--- tmux/.tmux/bundles/install.sh
+++ tmux/.tmux/bundles/install.sh
@@ -1,9 +1,5 @@
 # Initialize plugins
 
-# set -g @resurrect-dir '~/.tmux/tmp/resurrect'
-# run-shell ~/.tmux/bundles/tmux-resurrect/resurrect.tmux
-# set -g @continuum-restore 'on'
-# run-shell ~/.tmux/bundles/tmux-continuum/continuum.tmux
-run-shell ~/.tmux/bundles/tmux-open/open.tmux
-run-shell ~/.tmux/bundles/tmux-yank/yank.tmux
-run-shell ~/.tmux/bundles/tmux-copycat/copycat.tmux
+run-shell ~/.tmux/bundles/open/open.tmux
+run-shell ~/.tmux/bundles/yank/yank.tmux
+run-shell ~/.tmux/bundles/copycat/copycat.tmux
