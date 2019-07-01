26d8fdd7be4fb8bbd675851e147da2d32cb5fbdc
pmendelski <mendelski.pawel@gmail.com>
2019-07-01 20:20:04 +0200

    Add ubuntu server commands

---
+++ bash/.bash/plugins/update.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+if type "apt-get" &> /dev/null; then
+  update() {
+    echo "Updating all system packages"
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo snap refresh
+  }
+fi
