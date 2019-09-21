e27ec939ba7f287dba8288b68341a1e61072b97a
pmendelski <mendelski.pawel@gmail.com>
2019-09-21 15:16:48 +0200

    Add docker to ubuntuserver packages

--- _init/ubuntu-server/packages.sh
+++ _init/ubuntu-server/packages.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bashx
 
+key() {
+  curl -fsSL "$1" | sudo apt-key add -
+}
+
 repository() {
   sudo add-apt-repository -y "$1"
+  sudo apt-get update
 }
 
 package() {
@@ -57,3 +62,25 @@ package dnsutils
 
 # JSON parser
 package jq
+
+# Network
+package apt-transport-https
+package ca-certificates
+package gnupg-agent
+package software-properties-common
+
+# Docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt -y update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker "${USER}"
+su - "${USER}"
+# Docker compose https://docs.docker.com/compose
+docker-compose-version="$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+" | head -n 1)"
+sudo curl -L "https://github.com/docker/compose/releases/download/${docker-compose-version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/${docker-compose-version}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/${docker-compose-version}/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
