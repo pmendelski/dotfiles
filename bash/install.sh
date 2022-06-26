01c07aa24c8a1aea54261f8442a7785be8e07632
Paweł Mendelski <mendelski.pawel@gmail.com>
2022-06-26 10:49:49 +0200

    Fix macos install script

--- bash/install.sh
+++ bash/install.sh
@@ -6,13 +6,30 @@ initFile() {
   local -r target="$HOME/.$1"
   local -r host="${HOST:-$HOSTNAME}"
   if [ ! -f "$target" ]; then
-    cp "$file" "$target"
-    sed -i \
-      -e "s|@PROMPT_DEFAULT_USERHOST@|$USER@$host|" "$target" \
-      -e "s|@HOME@|$HOME|" "$target"
+    local -r content="S(cat "$file" | sed \
+      -e "s|@PROMPT_DEFAULT_USERHOST@|$USER@$host|" \
+      -e "s|@HOME@|$HOME|")"
+    echo "$content" >> "$target"
     printSuccess "Created: ~/.$1"
   else
     printInfo "File already exists. Skipping: ~/.$1"
+  fi
+}
+
+appendFile() {
+  local -r file="bash/_$1"
+  local -r target="$HOME/.$1"
+  local -r host="${HOST:-$HOSTNAME}"
+  local -r content="$(cat "$file" | sed \
+      -e "s|@PROMPT_DEFAULT_USERHOST@|$USER@$host|" \
+      -e "s|@HOME@|$HOME|")"
+  echo "$content" >> "$target"
+  if [ ! -f "$target" ]; then
+    printSuccess "Created: ~/.$1"
+  elif ! grep -q "$(echo "$content" | head -n 1)" "$target"; then
+    printSuccess "Appended: ~/.$1"
+  else
+    printInfo "File already exists and contains the entries. Skipping: ~/.$1"
   fi
 }
 
