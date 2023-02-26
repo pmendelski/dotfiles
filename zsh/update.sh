e369b884019a0a2c5382572eeb2218e8f38ffd5f
Paweł Mendelski <mendelski.pawel@gmail.com>
2023-02-26 19:22:02 +0100

    fix update script for zsh

--- zsh/update.sh
+++ zsh/update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
+declare -r DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && echo "$PWD")"
+source "$DIR/shared.sh"
+
 updatePlugins() {
   local -r dotfileDirs="$(find "$HOME/.zsh/deps" -mindepth 1 -maxdepth 1 -type d | sort)"
   for dir in $dotfileDirs; do
@@ -10,4 +13,5 @@ updatePlugins() {
   done
 }
 
+installPlugins
 updatePlugins
