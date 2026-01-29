29b4c9bb59b17a64153eb425182fadb3dac12520
Paweł Mendelski <mendelski.pawel@gmail.com>
2026-01-29 20:11:07 +0100

    Leave integration with sdkvm for backward compatibility

--- _sysinit/cloudvm/init.sh
+++ _sysinit/cloudvm/init.sh
@@ -104,3 +104,12 @@ fi
 #   gcloud components install alpha
 #   gcloud components install beta
 # fi
+
+if ! command -v mise &>/dev/null; then
+  echo -e "\n>>> Mise"
+  sudo install -dm 755 /etc/apt/keyrings
+  curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.asc 1>/dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+  sudo apt update -y
+  sudo apt install -y mise
+fi
--- _sysinit/rpi/init.sh
+++ _sysinit/rpi/init.sh
@@ -52,10 +52,21 @@ sudo apt install -y \
   dnsutils \
   apache2-utils
 
-echo -e "\n>>> Eza"
-sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
-sudo apt install -y eza
+if ! command -v eza &>/dev/null; then
+  echo -e "\n>>> Eza"
+  sudo mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
+fi
+
+if ! command -v mise &>/dev/null; then
+  echo -e "\n>>> Mise"
+  sudo install -dm 755 /etc/apt/keyrings
+  curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.asc 1>/dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+  sudo apt update -y
+  sudo apt install -y mise
+fi
